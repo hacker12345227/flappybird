51fa9a94f35ce1b34b139f837d831d96dcbc0890
hacker12345227 <Jarno.eulen12@gmail.com>
2026-03-24 18:20:22 +0100

    Update script.js

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -2,11 +2,27 @@ const list = document.getElementById("scoreList");
 
 let users = JSON.parse(localStorage.getItem("users")) || {};
 
+// maak array en sorteer
+let scores = [];
+
 for (let name in users) {
     let best = users[name].scores[0] || 0;
-
-    let li = document.createElement("li");
-    li.textContent = name + " - " + best;
-
-    list.appendChild(li);
+    scores.push({ name, score: best });
 }
+
+scores.sort((a, b) => b.score - a.score);
+
+// render
+scores.forEach(s => {
+    let li = document.createElement("li");
+    li.textContent = s.name + " - " + s.score;
+    list.appendChild(li);
+});
+
+// menu toggle
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
+
+menuButton.onclick = () => {
+    menu.style.left = menu.style.left === "0px" ? "-220px" : "0px";
+};
